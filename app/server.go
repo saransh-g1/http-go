0420ac861e1f62af9d355d64f3ed5df51cd50314
saranshgupta2711 <saranshkeshari9@gmail.com>
2024-12-28 10:44:35 +0530

    changes

--- app/server.go
+++ app/server.go
@@ -56,9 +56,8 @@ func resolveHeaders (conn net.Conn){
 	 fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(req.UserAgent()), req.UserAgent())
    }else if strings.Contains(req.URL.Path,"/files") {
      str:=strings.TrimPrefix(req.URL.Path,"/files")
-     args:=os.Args[0]
+     args:=os.Args[2]
      dirfn:=args+str
-     fmt.Println(dirfn,os.Args)
      file,err:=os.ReadFile(dirfn)
      if err!=nil{
        fmt.Println(err)
