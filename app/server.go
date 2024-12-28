a6a8e1072bcb6efc253531b3656aaf4e6626d771
saranshgupta2711 <saranshkeshari9@gmail.com>
2024-12-28 13:33:22 +0530

    changes

--- app/server.go
+++ app/server.go
@@ -48,7 +48,7 @@ func resolveHeaders (conn net.Conn){
      fmt.Println("error while reading", errr)
    }
     if req.Header.Get("Accept-Encoding")=="gzip"{
-     fmt.Fprintf(conn,"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip")
+     fmt.Fprintf(conn,"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\n")
      
       //has to be seen
      return
